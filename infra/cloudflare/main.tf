6df69f3211f2308eeec235e697656b176baa1d68
JD <jdk342@cornell.edu>
2026-09-24 19:03:52 -0400

    Format the ingestion Worker resource the way tofu fmt expects

--- infra/cloudflare/main.tf
+++ infra/cloudflare/main.tf
@@ -130,11 +130,11 @@ resource "cloudflare_queue" "dead_letter" {
 }
 
 resource "cloudflare_workers_script" "ingestion" {
-  account_id          = var.cloudflare_account_id
-  script_name         = local.ingestion_worker_name
-  main_module         = "ingestion-worker.js"
-  content_file        = local.ingestion_worker_bundle
-  content_sha256      = filesha256(local.ingestion_worker_bundle)
+  account_id     = var.cloudflare_account_id
+  script_name    = local.ingestion_worker_name
+  main_module    = "ingestion-worker.js"
+  content_file   = local.ingestion_worker_bundle
+  content_sha256 = filesha256(local.ingestion_worker_bundle)
   files = {
     "resvg.wasm" = {
       content_type = "application/wasm"
